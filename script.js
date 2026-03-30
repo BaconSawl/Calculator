d1bf93656d9fe15faf6c71ca9defad647be4fbdc
BaconSawl <ngolongvu306@gmail.com>
2026-03-30 15:48:34 +0700

    Functional Calcculator! Now it time to debug and fix the many bugs

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-let number1;
-let operator;
-let number2;
+let number1 = "";
+let operator = "";
+let number2 = "";
+
+const digitBtn = document.querySelectorAll(".digit");
+const display = document.querySelector("#display");
+const operatorBtn = document.querySelectorAll(".operator"); 
+const equalBtn = document.querySelector(".equal");
+
+operatorBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+        display.textContent += button.textContent;
+        operator = button.textContent;
+    })
+})
+
+digitBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+        // console.log(button.textContent);
+
+        //display.append(button.textContent);
+        display.textContent += button.textContent;
+
+        if (!operator) {
+            number1 += button.textContent;
+        } else {        
+            number2 += button.textContent;
+        }
+    })
+})
+
+equalBtn.addEventListener("click", () => {
+    const result = operate(Number(number1), operator, Number(number2));
+    console.log(result);
+
+    display.textContent = "";
+    display.append(result.toFixed(4)); // Round the number
+});
 
 function operate(number1, operator, number2) {
     switch (operator) {
